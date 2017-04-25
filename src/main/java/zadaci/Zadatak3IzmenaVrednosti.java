27ec4b037bceb2d374bf203b3211a5edda7b10e3
Nenad <norlovic@gmail.com>
2017-04-25 18:32:37 +0200

    Kreirane klase za zadatke

---
+++ src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
@@ -0,0 +1,35 @@
+package zadaci;
+
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+import java.io.IOException;
+
+/**
+ * Created by androiddevelopment on 25.4.17..
+ */
+public class Zadatak3IzmenaVrednosti {
+    public static void main(String[] args) {
+        ConnectionSource connectionSource = null;
+        // create our data-source for the database
+        try{
+            connectionSource = new JdbcConnectionSource("jdbc:sqlite:vozVagon.db");
+
+
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (connectionSource != null) {
+                try {
+                    connectionSource.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+
+    }
+}
